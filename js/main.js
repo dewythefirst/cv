3d041555b48ee82f6d6a6becc7a06accc8f724ec
Ilya <dewy.thefirst@gmail.com>
2019-05-19 22:48:24 +0300

    fixed height changes

--- js/main.js
+++ js/main.js
@@ -3,51 +3,20 @@ document.querySelector(".links a[name='portfolio']").addEventListener("click", p
 function portfolioHolder(e) {
     e.preventDefault();
     let holder = document.querySelector(".works-holder");
-    let maxHeight = 300;
-    let currentHeight = holder.offsetHeight;
-    let step = 1;
-    let interval = 1000;
-    slideElement(holder, 300, 500)
+    changeHeight(holder, 30, 500)
 }
 
-function slideElement(element, height, time) {
-    console.log("here");
-    if (!element)
-        return;
-    if (time == null) {
-        time = 1000;
-    }
-    if (height < 0) {
-        height = 0;
-    }
-    let currentHeight = element.offsetHeight;
-    let step = Math.floor((currentHeight - height) / time);
-    console.log(step);
-    if (height > currentHeight)
-        step *= -1;
-    console.log(currentHeight, height);
-    if (currentHeight !== height)
-        setTimeout(function (element, height, time, step) {
-            let currentHeight = element.offsetHeight;
-            element.style.height = currentHeight + step + "px";
-            slideElement(element, height, time);
-        }, 1, element, height, time, step);
+function changeHeight(element, to, duration) {
+    if (duration <= 0) return;
+    // console.log(element.offsetHeight, to);
+    if (element.offsetHeight === to)
+        to = 0;
+    var difference = to - element.offsetHeight;
+    var perTick = difference / duration * 10;
+    // console.log(difference, perTick, duration);
+    setTimeout(function () {
+        element.style.height = element.offsetHeight + perTick + "px";
+        if (element.offsetHeight === to) return;
+        changeHeight(element, to, duration - 10);
+    }, 10);
 }
-
-/*
-function portfolioHolder(e) {
-    e.preventDefault();
-    let holder = document.querySelector(".works-holder .works");
-    if (holder.classList.contains("active")) {
-        holder.classList.remove("active");
-        setTimeout(function (element) {
-            element.style.display = "none";
-        }, 100, holder)
-    } else {
-        holder.style.display = "block";
-        setTimeout(function (element) {
-            element.classList.add("active");
-        }, 100, holder);
-    }
-}
-*/
