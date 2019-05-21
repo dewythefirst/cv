4ed745606da266357141b3765084e36fd7906f80
Ilya <dewy.thefirst@gmail.com>
2019-05-21 23:26:18 +0300

    ticks sync

--- js/main.js
+++ js/main.js
@@ -24,13 +24,14 @@ function changeHeight(element, to, duration) {
 let passedTimeHolder = document.querySelector(".passed-time");
 let passedTimeCounter = function (date) {
     date = new Date(date);
-    // console.log(date);
     let dateNow = new Date();
+    // console.log(date, dateNow);
     let difference = new Date(dateNow - date);
     let days = Math.floor((dateNow.getTime() - date.getTime()) / (1000 * 60 * 60 * 24)),
         hours = difference.getHours(),
         minutes = difference.getMinutes(),
         seconds = difference.getSeconds();
     passedTimeHolder.innerText = days + " days " + ((hours < 10) ? "0" : "") + hours + ":" + ((minutes < 10) ? "0" : "") + minutes + ":" + ((seconds < 10) ? "0" : "") + seconds;
+    passedTimeHolder.style.animationPlayState = "running";
 };
-setInterval(passedTimeCounter, 1000, "2019/05/19 19:55 GMT+3");
+setInterval(passedTimeCounter, 1000, "2019/05/19 19:55 GMT+0300");
